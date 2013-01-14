ae6331d2acb53e7c1e1fd73b2a287e15ec275d85
Brad Mallow <briznad@gmail.com>
2013-01-14 10:15:36 -0800

    added dealing animation

--- assets/js/local/do.js
+++ assets/js/local/do.js
@@ -3,15 +3,15 @@
     /* load global objects */
 
     var debugMode = true, // prod = false, dev = true
-    	debugLog = function (msg) {
+    	debugLog = function (msg) { // throw messages to console.log only when in dev mode
 	    	if (debugMode) console.log(msg);
 	    },
-	    currentUsername = null, // dev - this should store something unique, such as username or user ID
-	    dealOrder = null, // stub for future Arr containing list of sorted keys representing the deal order, relative to the current client user
-    	currentRound = 0,
-    	$body = null,
+	    currentUser = null, // dev - this should store something unique, such as username or user ID
+	    gameReferee = null,
+    	currentRound = 0, // start the game at round 0, aka high card deal
+    	$body = null, // stubs for future jQuery object caching
     	$createGameForm = null,
-    	$rounds = null,
+    	$roundsSelector = null,
     	$scoreboardContainer = null,
     	$cardTableContainer = null,
     	$showHideScoreboardLink = null,
@@ -20,51 +20,163 @@
     	game = {
     		started: false,
         	rounds: [],
-        	players: [],
+        	playerList: [],
         	playerCount: function () {
-				return 'players-' + this.players.length;
+				return 'players-' + this.playerList.length;
 			},
         	scoreboardPlayerThreshold: function () {
-				if (this.players.length <= 6) {
+				if (this.playerList.length <= 6) {
 					return 'span4 offset8';
 				} else {
 					return 'span5 offset7';
 				}
 			}
         },
-        ferrisWheel = function (loopedArr, increment) { // copy the indices of the passed array as the values of a new array, then rotate the items in the new array, FORWARD TO BACK, as if in a loop, and return the new adjusted array
+        printScoreboard = function () { // print scoreboard
+
+    		$scoreboardContainer.html(ich.scoreboard(game));
+
+		},
+    	printCardTable = function () { // print card table
+
+			$cardTableContainer.html(ich.cardTable(game));
+
+		},
+		printScoreboardCardTable = function () { // print both at once
+
+			printScoreboard(); // display scoreboard
+
+	    	printCardTable(); // display card table
+
+		},
+        ferrisWheel = function (loopedArr, increment) { // rotate the passed array as if in a loop, HIGHER to LOWER / BACK to FRONT, incrementing by the passed number
 
         	debugLog('ferris wheel called');
 
-        	var tempIncrement = parseInt(increment),
-        		tempArr = _.keys(loopedArr);
+        	var tempIncrement = parseInt(increment);
 
-        	_.each(tempArr, function (value, index) {
-        		tempArr[index] = parseInt(value);
-        	});
-
-        	if (tempIncrement < 0) tempIncrement = tempArr.length - tempIncrement; // if passed a negative number, move "forward"
+        	if (tempIncrement < 0) tempIncrement = loopedArr.length - tempIncrement; // if passed a negative number, move "backward"
 
         	_.times(tempIncrement, function () {
 
-        		tempArr.push(tempArr.shift());
+        		loopedArr.push(loopedArr.shift());
 
         	});
 
-        	return tempArr;
-
         },
-        calibrateTable = function () { // assign deal order based on position relative to the current client player facilitating rotating client player to the bottom of the table
+        rotateTable = function () { // rotate the each of "game.rounds[].players" arrays so that the client player is at index 0, and will therefore be "seated" at the bottom of the table
 
-        	debugLog('calibrate table called');
+			debugLog('rotate table called');
 
-        	var playerOffset = _.indexOf(game.players, _.find(game.players, function (val) { return val.username == currentUsername; }));
+			var tempFind = _.find(game.playerList, function (val) { return val.currentUser; });
 
-        	dealOrder = ferrisWheel(game.players, playerOffset);
+	    	var tempPlayerOffset = _.indexOf(game.playerList, tempFind);
 
-        	debugLog(dealOrder);
+	    	_.each(game.rounds, function (value, index) {
 
-        },
+	    		ferrisWheel(value.players, tempPlayerOffset);
+
+	    	});
+
+		},
+		sortCardsByNumber = function (cardsToSort, sortOrder) {
+
+			var tempArr = _.sortBy(cardsToSort, function (value) {
+
+				switch(value.card) {
+					case 'two':
+						return 1;
+					break;
+					case 'three':
+						return 2;
+					break;
+					case 'four':
+						return 3;
+					break;
+					case 'five':
+						return 4;
+					break;
+					case 'six':
+						return 5;
+					break;
+					case 'seven':
+						return 6;
+					break;
+					case 'eight':
+						return 7;
+					break;
+					case 'nine':
+						return 8;
+					break;
+					case 'ten':
+						return 9;
+					break;
+					case 'jack':
+						return 10;
+					break;
+					case 'queen':
+						return 11;
+					break;
+					case 'king':
+						return 12;
+					break;
+					case 'ace':
+						return 13;
+					break;
+				}
+
+			});
+
+			if (sortOrder == 'descending') {
+				return tempArr.reverse();
+			} else {
+				return tempArr;
+			}
+
+		},
+		sortCardsBySuit = function (cardsToSort) {
+
+			return _.sortBy(cardsToSort, function (value) {
+
+				switch(value.suit) {
+					case 'spades':
+						return 1;
+					break;
+					case 'diamonds':
+						return 2;
+					break;
+					case 'clubs':
+						return 3;
+					break;
+					case 'hearts':
+						return 4;
+					break;
+				}
+
+			});
+
+		},
+		sortClientPlayersCards = function () { // iterate through the current client player's cards for each round where there are 2 or more cards, and sort them so they look nice
+
+			_.each(game.rounds, function (value, index) {
+
+				if (value.cards > 1) {
+
+					_.each(value.players, function (value, index) {
+
+						if (value.currentUser == true) {
+
+							value.cards = sortCardsBySuit(sortCardsByNumber(value.cards));
+
+						}
+
+					});
+
+				}
+
+			});
+
+		},
         dealDelay = function (delayedFunction) {
 
 			dealDelayQueue.push(delayedFunction);
@@ -73,7 +185,7 @@
 
 				if ( dealDelayQueue.length === 0 ) {
 
-					clearTimeout(dealDelayTimer);
+					clearInterval(dealDelayTimer);
 
 					dealDelayTimer = null;
 
@@ -85,7 +197,7 @@
 
 			}
 
-			if (!dealDelayTimer) dealDelayTimer = setInterval(executeDelayedFunction, 250);
+			if (!dealDelayTimer) dealDelayTimer = setInterval(executeDelayedFunction, 200);
 
 	    },
         updateCurrentRound = function () { // run at the beginning of each round, starting with the first round fo play
@@ -101,33 +213,19 @@
         		_.each(game.rounds, function (value, index) {
 
 					if (index === currentRound) {
-						game.rounds[index]['currentRound'] = true;
+						value['started'] = true;
+						value['currentRound'] = true;
 					} else {
-						game.rounds[index]['currentRound'] = false;
+						value['currentRound'] = false;
 					}
 
 				});
 
 				$body.removeClass('round-high-card-deals round-' + (currentRound - 1) ).addClass('round-' + currentRound);
 
+				printScoreboardCardTable(); // print table
+
         	}
-
-		},
-    	printScoreboard = function () { // print scoreboard
-
-    		$scoreboardContainer.html(ich.scoreboard(game));
-
-		},
-    	printCardTable = function () { // print card table
-
-			$cardTableContainer.html(ich.cardTable(game));
-
-		},
-		printScoreboardCardTable = function () { // print both at once
-
-			printScoreboard(); // display scoreboard
-
-	    	printCardTable(); // display card table
 
 		},
 		initializeEventListeners = function () {
@@ -147,25 +245,23 @@
 
 			_.times(game.rounds[currentRound].cards, function (n) { // iterate through the number of cards needed for the current hand
 
-				_.each(dealOrder, function (value, index) { // iterate through each player in the order in which they sit at the table
+				_.each(game.rounds[currentRound].players, function (value, index) { // iterate through each player
 
-					var currentPlayer = game.rounds[currentRound].cardsDealt[value];
-
-					debugLog(currentPlayer);
+					var currentCardSelector = '.player:eq(' + index + ') .card:eq(' + n + ')';
 
 		    		dealDelay(function () {
 
-		    			var currentCard = n;
-
-		    			currentPlayer.cards[currentCard].displayed = true;
-
-			    		printCardTable();
+		    			$cardTableContainer.find(currentCardSelector).addClass('dealt');
 
 		    		});
 
 		    	});
 
 			});
+
+			dealDelay(function () { /* dummy beat */ });
+
+			dealDelay(function () { $cardTableContainer.find('.card-table').addClass('deal-complete'); });
 
 		},
         freshShuffledDeck = function () {
@@ -215,13 +311,17 @@
 
     /* /establish connection to Firebase */
 
-    /* create game */
+    //*********************************************************************************************
+    //
+    // "Server" Code - code to be run by 1 player per game, as if by a central server
+    //
+    //*********************************************************************************************
 
-    function createGame() { // if you're the first person, create a new game
+    var createGame = function () { // if you're the first person, create a new game
 
 	    debugLog('create game called');
 
-    	$rounds.change(function() {
+    	$roundsSelector.change(function() {
 
     		$(this).next('.label').text('');
 
@@ -240,153 +340,175 @@
     	$createGameForm.submit(function(e) {
     		e.preventDefault();
 
-    		var rounds = parseInt( $rounds.val() ),
+    		gameReferee = true; // indicate that you're the game referee, and you'll be doing running the "server" functions from here on out
+
+    		var rounds = parseInt( $roundsSelector.val() ),
     			player = $(this).find('#player').val();
 
-    		game.players.push({ name: player, username: player.replace(/\s/g, '').toLowerCase(), score: 0 });
+    		game.playerList.push({ name: player, username: player.replace(/\s/g, '').toLowerCase(), score: 0 });
 
     		_.times(( rounds * 2 ) - 1, function(n) {
 
-    			var roundModel = {
+    			var tempRoundModel = {
 						hands: [],
 						cards: null,
-						cardsDealt: null
+						players: null
 					};
+
+				if (n == 0) tempRoundModel.started = true; // if building the 1st round add the "started" flag for the scoreboard
 
     			if ($createGameForm.find('input[name=order]:checked').val() === 'low') { // for low/high/low games, increment the rounds starting at 1, going up to [# of rounds], then back down to 1
 
     				if (n < rounds) {
-    					roundModel.cards = n + 1;
+    					tempRoundModel.cards = n + 1;
     				} else {
-    					roundModel.cards = rounds + ( rounds - ( n + 1 ) );
+    					tempRoundModel.cards = rounds + ( rounds - ( n + 1 ) );
     				}
 
     			} else { // for high/low/high games, increment the rounds starting at [# of rounds], going down to 1, then back up to [# of rounds]
 
     				if (n < rounds) {
-    					roundModel.cards = rounds - n;
+    					tempRoundModel.cards = rounds - n;
     				} else {
-    					roundModel.cards = ( n + 2 ) - rounds;
+    					tempRoundModel.cards = ( n + 2 ) - rounds;
     				}
 
     			}
 
-    			roundModel.hands.length = roundModel.cards; // set the length of the currently empty "hands" array to the number of cards in the round
+    			tempRoundModel.hands.length = tempRoundModel.cards; // set the length of the currently empty "hands" array to the number of cards in the round
 
-    			game.rounds.push(roundModel);
+    			game.rounds.push(tempRoundModel);
 
     		});
 
-	    	game.rounds.unshift({ cards: 1, cardsDealt: null, highCard: true, currentRound: true }); // add initial high card round in the first spot in the "rounds" array
+	    	game.rounds.unshift({ cards: 1, players: null, highCardDeals: true }); // add initial high card round in the first spot in the "rounds" array
 
-			startGame();
+			addPlayers();
 
     	});
 
-    }
+    },
+    addPlayers = function () { // join all players to the game
 
-    /* /create game */
+    	debugLog('add players called');
 
-    /* start game */
-
-    function startGame() { // when other players join, start a game
-
-    	debugLog('start game called');
 
     	// dev code
 
-    	// set current user
-		// in prod code this should be set by a check using Firebase Auth
-		currentUsername = game.players[0].username;
-
-		// might need to find a better way to do this for prod
-		game.players[0]['currentUser'] = true;
-
-
     	// add dummy players
-		var dummyPlayers = ['Nancy', 'Steve', 'Cindy', 'George', 'Stan', 'Brenda', 'Greg'];
+		var dummyPlayers = ['Player 2', 'Player 1', 'Player 4', 'Player 5', 'Player 6', 'Player 7', 'Player 8'];
 
-		_.times(5, function (n) {
+		_.times(7, function (n) {
 			if (n <= 1) {
-				game.players.unshift({ name: dummyPlayers[n], username: dummyPlayers[n].replace(/\s/g, '').toLowerCase(), score: 0 });
+				game.playerList.unshift({ name: dummyPlayers[n], username: dummyPlayers[n].replace(/\s/g, '').toLowerCase(), score: 0 });
 			} else {
-				game.players.push({ name: dummyPlayers[n], username: dummyPlayers[n].replace(/\s/g, '').toLowerCase(), score: 0 });
+				game.playerList.push({ name: dummyPlayers[n], username: dummyPlayers[n].replace(/\s/g, '').toLowerCase(), score: 0 });
 			}
 		});
 
 		// /end dev code
 
-		// once all players have been added to the game object…
 
-		$body.addClass('player-count-' + game.players.length); // identifiy the number of players sitting at the table
+		dealTheRounds();
 
-		calibrateTable(); // save table rotation offset so that the client player is "seated" at the bottom of the table
+    },
+    dealTheRounds = function () { // once all players have joined, deal the cards for all rounds
 
+    	debugLog('deal the rounds called');
 
-		_.each(game.rounds, function (value, index, list) { // deal out each round, to each player, then select trump
+    	_.each(game.rounds, function (value, index, list) { // deal out each round, to each player, then select trump
 
 			var thisRound = value, // make some cache vars
 				deck = freshShuffledDeck(); // grab a shuffled deck for each round's deal
 
-			value.cardsDealt = _.map(game.players, function (item) { // populate each round with [cards dealt], a list of players with the blank "cards"
-				return { name: item.name, username: item.username, currentUser: item.currentUser, cards: [] };
+			value.players = _.map(game.playerList, function (item) { // populate each round with a players arr; a list of players with blank "cards"
+				return { name: item.name, username: item.username, cards: [], bid: ' ', take: 0 };
 			});
 
-			_.each(value.cardsDealt, function (value, index, list) {
-
+			_.each(value.players, function (value, index, list) { // pass out the number of cards for each round to each player
 				value.cards = deck.splice(0, thisRound.cards);
-
 			});
 
-			if (!thisRound.highCard || thisRound.highCard === undefined) thisRound['trump'] = deck.shift();
+			if (!thisRound.highCardDeals || thisRound.highCardDeals === undefined) thisRound['trump'] = deck.shift();
 
 		});
 
-		initializeEventListeners();
+    	setupGame();
 
-    	$body.addClass('start-game round-high-card-deals');
+    },
+    scoreHighCardDeal = function () {
+
+
+
+    };
+
+    //*********************************************************************************************
+    //
+    // "Client" Code - code to be run by each player
+    //
+    //*********************************************************************************************
+
+    var setupGame = function() { // once all server functions have run, setup the game relative to each client player
+
+    	debugLog('setup game called');
+
+
+    	// dev code
+
+    	// set current user
+		// MUST find a better way to do this for prod
+
+		// set currentUser for the current client user in the players arr and each round
+		game.playerList[2]['currentUser'] = true;
+
+		_.each(game.rounds, function (value, index) {
+			value.players[2]['currentUser'] = true;
+		});
+
+		// in prod code this should be set by a check using Firebase Auth
+		currentUser = game.playerList[2].username;
+
+		// /end dev code
+
+		sortClientPlayersCards(); // iterate through the current client player's cards for each round where there are 2 or more cards, and sort them so they look nice
+
+    	rotateTable(); // rotate the each of "game.rounds[].players" arrays so that the client player is at index 0, and will therefore be "seated" at the bottom of the table
+
+    	$body.addClass('start-game round-high-card-deals player-count-' + game.playerList.length); // identifiy the number of players sitting at the table
+
+    	initializeEventListeners();
 
     	printScoreboardCardTable(); // display scoreboard and card table
 
-    	showHighCardDeal(); // load high card deal show function to show first dealer
-
     	debugLog(' /end start game');
 
-    }
+    	showHighCardDeal(); // load high card deal show function to show first dealer
 
-    /* /start game */
+    },
+    showHighCardDeal = function () { // deal 1 card to each player to choose first dealer
 
-    /* show high card deals */
-
-    function showHighCardDeal() { // deal 1 card to each player to choose first dealer
-
-    	debugLog('show high card called');
-
-    	debugLog(game);
+    	debugLog('show high card deal called');
 
 		game.started = true; // since this is the first round, indicate that the game has started
 
 		var t = setTimeout(dealSomeCards, 1000); // deal some cards after a short delay
 
-    }
+		if (gameReferee) scoreHighCardDeal();
 
-    /* /show high card deals */
+		debugLog(game);
 
-    /* show each round */
-
-    function showRound() { // deal each round
+    },
+    showRound = function () { // deal each round
 
     	debugLog('show round called');
 
 		updateCurrentRound(); // update current round
 
-		printScoreboardCardTable();
-
 		var t = setTimeout(dealSomeCards, 1000); // deal some cards after a short delay
 
     	debugLog(game);
 
-    }
+    };
 
     /* /show each round */
 
@@ -397,7 +519,7 @@
     	// cache DOM elements
     	$body = $('body'),
     	$createGameForm = $('#createGameForm'),
-    	$rounds = $('#rounds'),
+    	$roundsSelector = $('#roundsSelector'),
     	$scoreboardContainer = $('#scoreboardContainer'),
     	$cardTableContainer = $('#cardTableContainer'),
     	$showHideScoreboardLink = $('#showHideScoreboardLink');
@@ -406,11 +528,13 @@
 
     	// dev
     	$('#previousRoundLink').click(function () {
-    		currentRound--;
+    		dealDelayQueue = [];
+    		currentRound = currentRound - 2;
     		showRound();
     	});
 
     	$('#advanceRoundLink').click(function () {
+    		dealDelayQueue = [];
     		showRound();
     	});
 
